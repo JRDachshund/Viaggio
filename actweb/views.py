3b915aee72cc60e1d01f927634b48722af123cc2
JRDachshund <jrluehning@gmail.com>
2026-06-15 08:27:58 +0200

    Created separate subpages

--- actweb/views.py
+++ actweb/views.py
@@ -4,4 +4,26 @@ views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET", "POST"])
 def home():
-    return "Hello World"
+    return render_template('homePage.html')
+
+@views.route("/map", methods=["GET", "POST"])
+def map():
+    return render_template('map.html')
+
+@views.route("/destinationSelection", methods=["GET", "POST"])
+def destinationSelection():
+    return render_template('destinationSelection.html')
+
+@views.route("/transportSelection", methods=["GET", "POST"])
+def transportSelection():
+    return render_template('transportSelection.html')
+
+@views.route("/accommodationSelection", methods=["GET", "POST"])
+def accommodationSelection():
+    return render_template('accommodationSelection.html')
+
+@views.route("/tripSummary", methods=["GET", "POST"])
+# Hier sieht man die Übersicht über die geplante Reise und kann Unterkünfte buchen
+def tripSummary(): 
+    return render_template('tripSummary.html')
+
